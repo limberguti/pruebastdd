6788ec24a35968a2de5a2f1f23adcd3523c53936
Johnny Loachamin <57681011+JohnnyLs@users.noreply.github.com>
2024-03-07 09:45:43 -0500

    Update searchPersonal.spec.ts

--- client/src/PruebasTDD/searchPersonal/searchPersonal.spec.ts
+++ client/src/PruebasTDD/searchPersonal/searchPersonal.spec.ts
@@ -5,30 +5,31 @@ import { expect } from '@jest/globals';
 
 const feature = loadFeature('src/PruebasTDD/searchPersonal/searchPersonal.feature');
 
+// Instancia del servicio de búsqueda de docentes
 const docenteService = new DocenteService();
 
+// Definición de la feature y escenarios de prueba
 defineFeature(feature, (test) => {
   let apellidos: string;
-  let originalData: any[];
 
-
+  // Escenario de prueba: Buscar un docente por sus apellidos
   test('Buscar un docente por sus apellidos', ({ given, and, when, then }) => {
-    given('que el endpoint "/docente" está disponible para buscar docentes', async () => {
-      // Puedes hacer algunas inicializaciones aquí si es necesario
-    });
+    given('que el endpoint "/docente" está disponible para buscar docentes', async () => {});
 
-    and('existen docentes en la base de datos', async () => {
-      
-    });
+    and('existen docentes en la base de datos', async () => {});
 
     when('hago una solicitud GET a "/docente" con el parámetro de búsqueda "ApellidosDocente"', async () => {
+      // Definición del parámetro de búsqueda
       apellidos = 'ApellidosDocente';
+      
+      // Llamada al método para buscar docentes por apellidos
       await docenteService.buscarDocentesPorApellidos(apellidos);
     });
 
     then('debería recibir una respuesta con los docentes que coinciden con los apellidos', async () => {
+      // Consulta a la base de datos para obtener los docentes que coinciden con los apellidos
       const result = await db.query('SELECT * FROM docente WHERE APELLIDOS LIKE ?', [`%${apellidos}%`]);
-      // Verifica que el resultado coincida con tus expectativas
+      
       expect(result).toBeDefined();
     });
   });
